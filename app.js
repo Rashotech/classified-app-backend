2d3e70f3025fb429a6248ab3482a0fe816ddc18f
Rasheed Ayoade <rashotech@gmail.com>
2022-02-06 11:50:26 +0100

    app

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const express = require('express');
 const httpStatus = require('http-status');
 const passport = require('passport');
 const cors = require("cors");
+const compression = require("compression");
 const { jwtStrategy } = require('./config/passport');
 const AuthRoutes = require('./routes/auth.route');
-const MonoRoutes = require('./routes/mono.route');
+const AdsRoutes = require('./routes/ads.route');
+const CategoryRoutes = require('./routes/category.route');
+const ChatRoutes = require('./routes/message.route');
 
 const ApiError = require('./utils/ApiError');
 const { errorConverter, errorHandler } = require('./middleware/error');
@@ -20,15 +23,22 @@ const corsOptions = {
 };  
 app.use(cors(corsOptions));
 
+// app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(compression());
+
+app.use(express.static(__dirname + "/public"));
+app.use("/uploads", express.static("uploads"));
 
 // jwt authentication
 app.use(passport.initialize());
 passport.use('jwt', jwtStrategy);
 
-app.use('/api', MonoRoutes);
 app.use('/api/auth', AuthRoutes);
+app.use('/api/ads', AdsRoutes);
+app.use('/api/chat', ChatRoutes);
+app.use('/api/category', CategoryRoutes);
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
--- index.js
+++ index.js
@@ -8,6 +8,44 @@ server = app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
 
+const io = require("socket.io")(server, { pingTimeout: 30000 });
+
+// Socket Io Instance & Events
+io.on("connection", socket => {
+  console.log('User connected');
+
+  socket.on("setup", userData => {
+      socket.join(userData._id);
+      socket.emit("connected");
+  })
+
+  socket.on("joinRoom", room =>{ 
+    socket.join(room)
+  });
+
+  socket.on("typing", room => {
+    socket.in(room).emit("typing")
+  });
+  socket.on("stop typing", room => {
+    socket.in(room).emit("stop typing")
+  });
+  socket.on("notification received", room => socket.in(room).emit("notification received"));
+
+  socket.on("newMessage", newMessage => {
+    var chat = newMessage.data.chat;
+
+    // socket.in(chat._id).emit("messageReceived", newMessage);
+
+    if(!chat.users) return console.log("Chat.users not defined");
+
+    chat.users.forEach(user => {
+        
+        if(user._id == newMessage.data.sender._id) return;
+        socket.in(user._id).emit("messageReceived", newMessage);
+    })
+  })
+});
+
 const exitHandler = () => {
   if (server) {
     server.close(() => {
